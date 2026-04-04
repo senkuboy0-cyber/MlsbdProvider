e869a697a6b8a5ae5a6cf4af815e3dbbd8a63d9f
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-04 20:44:02 +0600

    Fix: add language and tvTypes metadata

--- MlsbdProvider/build.gradle.kts
+++ MlsbdProvider/build.gradle.kts
@@ -4,4 +4,16 @@ cloudstream {
     description = "MLSBD - The Largest Movie Link Store of Bangladesh"
     authors = listOf("senkuboy0-cyber")
     iconUrl = "https://mlsbd.co/wp-content/uploads/2023/01/cropped-MLSBD-LOGO-32x32.png"
+    language = "bn"
+    status = ExtensionStatus.Beta
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "Others"
+    )
+}
+
+android {
+    namespace = "com.mlsbd"
 }
