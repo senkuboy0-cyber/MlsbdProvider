3fe94ba8e4f70eaad2bed0bc2d6d55a7f81b6d54
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-04 20:48:12 +0600

    Fix: add language bn and tvTypes

--- MlsbdProvider/build.gradle.kts
+++ MlsbdProvider/build.gradle.kts
@@ -1,17 +1,11 @@
 cloudstream {
     setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/senkuboy0-cyber/MlsbdProvider")
-    version = 1
+    version = 2
     description = "MLSBD - The Largest Movie Link Store of Bangladesh"
     authors = listOf("senkuboy0-cyber")
     iconUrl = "https://mlsbd.co/wp-content/uploads/2023/01/cropped-MLSBD-LOGO-32x32.png"
     language = "bn"
-    status = ExtensionStatus.Beta
-    tvTypes = listOf(
-        "Movie",
-        "TvSeries",
-        "Anime",
-        "Others"
-    )
+    tvTypes = listOf("Movie", "TvSeries", "Anime", "Others")
 }
 
 android {
